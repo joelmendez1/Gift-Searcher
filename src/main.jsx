ed8a96319a22938105d6d68ac4358ccd3697903e
Joel Mendez <joel.mendez@outlook.es>
2022-12-27 19:25:07 -0300

    initialize app

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GiftExpertApp } from './GiftExpertApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <GiftExpertApp />
+)
