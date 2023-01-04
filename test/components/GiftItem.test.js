4e6b0c4178d68a3bc6d1195d1cd03dfcb85b73a0
Joel Mendez <joel.mendez@outlook.es>
2023-01-04 16:56:20 -0300

    add test to getGifs function

--- test/components/GiftItem.test.js
+++ test/components/GiftItem.test.js
@@ -1,15 +1,36 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components/GifItem"
 
 describe('Test in GifItem component', () => {
+  const title = "Titulo";
+  const url = "https://one-punch.com/saitama.jpg";
+
   test('Should match snapshot', () => {
-    const title = "Titulo";
-    const url = "https://one-punch.com/saitama.jpg";
     const { container } = render
       (
         <GifItem title={title} url={url} />
-      )
+      );
 
     expect(container).toMatchSnapshot();
+  })
+
+  test('Should show image with correct url', () => {
+    const { container } = render
+      (
+        <GifItem title={title} url={url} />
+      );
+    const { src, alt } = screen.getByRole('img');
+
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  })
+
+  test('Should show title in component', () => {
+    const { container } = render
+      (
+        <GifItem title={title} url={url} />
+      );
+
+    expect(screen.getByText(title)).toBeTruthy();
   })
 })
